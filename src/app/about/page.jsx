1c368f45913c3c8aee426371aff61426286d8d58
zjfsharp <zjfsharp@gmail.com>
2024-06-04 23:22:15 +0800

    homepage 样式

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const AboutPage = () => {
-    return <div>AboutPage!</div>;
+    return (
+      <div>
+        <div className={styles.imgContainer}>
+          {/* <Image src="/about.png" alt="" fill /> */}
+          <Image 
+            src="https://images.pexels.com/photos/20631973/pexels-photo-20631973/free-photo-of-a-vase-with-dried-flowers-on-a-table.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+            alt=""
+            fill />
+        </div>
+        
+      </div>
+    )
   };
   
   export default AboutPage;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,15 @@
+import styles from "./footer.module.css"
+
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className={styles.container}>
+            <div className={styles.logo}>
+                lamadev
+            </div>
+            <div className={styles.text}>
+                Lama creative thoughts agency @ All rights reserved.
+            </div>
+        </div>
     )
 }
 
